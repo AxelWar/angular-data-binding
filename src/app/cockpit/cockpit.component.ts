eb9aa0ce17b4613c061cf3660d4bc3ae2795d14c
Axel Warneke <Axel.Warneke@ey.com>
2020-04-29 19:03:02 -0300

    second commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+//import { EventEmitter } from "protractor";
+
+@Component({
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"]
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output("bpCreated") blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
+  newServerName = "";
+  newServerContent = "";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  onAddServer(nameInput) {
+    console.log(nameInput);
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+}
